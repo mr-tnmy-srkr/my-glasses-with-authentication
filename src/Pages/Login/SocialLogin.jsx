214a0a1c7d8942be3986c2b6b426b74d0277c45e
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-10-02 18:56:15 +0530

    completed

--- src/Pages/Login/SocialLogin.jsx
+++ src/Pages/Login/SocialLogin.jsx
@@ -2,7 +2,13 @@ import { useContext } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import { FaGoogle,FaFacebook,FaGithub,FaSquareXTwitter,FaYCombinator } from "react-icons/fa6";
+import {
+  FaGoogle,
+  FaFacebook,
+  FaGithub,
+  FaSquareXTwitter,
+  FaYCombinator,
+} from "react-icons/fa6";
 
 const SocialLogin = () => {
   const navigate = useNavigate();
@@ -24,36 +30,46 @@ const SocialLogin = () => {
     <>
       <div className="divider">continue with</div>
       <div className="grid grid-cols-5 gap-3">
-        <button
-          onClick={() => handleSocialLogin(googleLogin)}
-          className="btn btn-sm btn-neutral"
-        >
-        <FaGoogle/>
-        </button>
-        <button
-          onClick={() => handleSocialLogin(facebookLogin)}
-          className="btn btn-sm btn-neutral">
-         <FaFacebook/>
-        </button>
-        <button
-          onClick={() => handleSocialLogin(githubLogin)}
-          className="btn btn-sm btn-neutral"
-        >
-        <FaGithub/>
-        </button>
-        <button
-          onClick={() => handleSocialLogin(twitterLogin)}
-          className="btn btn-sm btn-neutral"
-        >
-          <FaSquareXTwitter/>
-        </button>
-        <button
-          onClick={() => handleSocialLogin(yahooLogin)}
-          className="btn btn-sm btn-neutral"
-        >
-        <FaYCombinator/>
-        </button>
-        
+        <div className="tooltip tooltip-bottom" data-tip="Google">
+          <button
+            onClick={() => handleSocialLogin(googleLogin)}
+            className="btn btn-sm btn-neutral"
+          >
+            <FaGoogle />
+          </button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="Facebook">
+          <button
+            onClick={() => handleSocialLogin(facebookLogin)}
+            className="btn btn-sm btn-neutral"
+          >
+            <FaFacebook />
+          </button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="Github">
+          <button
+            onClick={() => handleSocialLogin(githubLogin)}
+            className="btn btn-sm btn-neutral"
+          >
+            <FaGithub />
+          </button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="Twitter">
+          <button
+            onClick={() => handleSocialLogin(twitterLogin)}
+            className="btn btn-sm btn-neutral tooltip tooltip-bottom"
+          >
+            <FaSquareXTwitter />
+          </button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="Yahoo">
+          <button
+            onClick={() => handleSocialLogin(yahooLogin)}
+            className="btn btn-sm btn-neutral"
+          >
+            <FaYCombinator />
+          </button>
+        </div>
       </div>
     </>
   );
